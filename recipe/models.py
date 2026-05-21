33ffd4dc60532f5ae2bf482c2f9105dd57b4a750
katyadyadenko <katerina.dyadenko@gmail.com>
2026-05-21 16:45:51 +0300

    Add Category model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __iter__(self):
+        return iter([self.name])
+
+    def __str__(self):
+        return self.name
